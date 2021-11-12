0d3800fa8079df9d491179c5d3836bfd8f0e0dd1
“CaioVinids” <caiovinids@gmail.com>
2021-11-12 19:22:09 -0300

    Criacao das Activitys de Cachorro e Gato

---
+++ app/src/main/java/com/example/appcuidapet/HigieneDogActivity.java
@@ -0,0 +1,14 @@
+package com.example.appcuidapet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HigieneDogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_higiene_dog);
+    }
+}
---
+++ app/src/main/java/com/example/appcuidapet/HigieneGatoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appcuidapet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HigieneGatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_higiene_gato);
+    }
+}
